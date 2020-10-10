f9a3643c133739ee48ae23618ec218958c660adf
mhata2000 <50744052+mhata2000@users.noreply.github.com>
2020-10-10 21:26:37 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,29 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     キャッチャー.change(LedSpriteProperty.X, -1)
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
     game.pause()
     basic.showNumber(game.score())
     game.resume()
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     キャッチャー.change(LedSpriteProperty.X, 1)
 })
-let キャッチャー : game.LedSprite = null
+let キャッチャー: game.LedSprite = null
 キャッチャー = game.createSprite(2, 4)
 let りんご = game.createSprite(randint(0, 4), 0)
 basic.pause(100)
 game.setScore(0)
-basic.forever(function on_forever() {
+basic.forever(function () {
     if (りんご.get(LedSpriteProperty.Y) == 4) {
         りんご.set(LedSpriteProperty.Y, 0)
         りんご.set(LedSpriteProperty.X, randint(0, 4))
     } else {
         りんご.change(LedSpriteProperty.Y, 1)
     }
-    
-    basic.pause(1000)
+    basic.pause(500)
 })
-basic.forever(function on_forever2() {
+basic.forever(function () {
     if (キャッチャー.isTouching(りんご)) {
         game.addScore(1)
         りんご.set(LedSpriteProperty.Y, 0)
@@ -34,5 +33,4 @@ basic.forever(function on_forever2() {
         りんご.delete()
         キャッチャー.delete()
     }
-    
 })
